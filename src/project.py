bb27fdbc73eb138c3f4d1f42b038f7b928f76449
KyuumiNya <middiemeyer@gmail.com>
2023-11-30 14:52:25 -0600

    Added point collection mechanism and text

--- src/project.py
+++ src/project.py
@@ -1,11 +1,13 @@
 import pygame
 import math
+import random
+import time
 
 class Player():
-    def __init__(self, pos=(0, 0), size=30):
+    def __init__(self, pos=(0, 0), size=90):
         self.pos = list(pos)
         self.size = size
-        self.color = pygame.Color(0, 255, 0)  # Green color
+        self.color = pygame.Color(0, 255, 0)
         self.surface = self.update_surface()
 
     def update_surface(self):
@@ -27,31 +29,119 @@ class Player():
         if keys[pygame.K_d]:
             self.pos[0] += speed
 
+class SmallCircle():
+    def __init__(self, pos, size):
+        self.pos = pos
+        self.size = size
+        self.color = pygame.Color(random.randrange(256), random.randrange(256), random.randrange(256))
+        self.surface = self.update_surface()
+
+    def update_surface(self):
+        surf = pygame.Surface((self.size, self.size), pygame.SRCALPHA)
+        pygame.draw.circle(surf, self.color, (self.size // 2, self.size // 2), self.size // 2)
+        return surf
+
+    def draw(self, surface):
+        surface.blit(self.surface, self.pos)
+
+class Game():
+    def __init__(self, resolution):
+        self.resolution = resolution
+        self.screen = pygame.display.set_mode(resolution)
+        pygame.display.set_caption("Slime Collection!")
+        self.clock = pygame.time.Clock()
+        self.dt = 0
+        self.points = 0
+        self.player = Player(pos=(resolution[0] // 2, resolution[1] // 2), size=90)
+        self.small_circles = self.generate_small_circles(10)
+        self.win = False
+        self.win_timer = 0
+        self.win_display_time = 3
+        self.flash_duration = 1.0  # Adjusted flash duration
+        self.last_flash_time = 0
+
+    def generate_small_circles(self, count):
+        small_circles = []
+        for _ in range(count):
+            x = random.randrange(self.resolution[0])
+            y = random.randrange(self.resolution[1])
+            size = random.randint(10, 30)
+            small_circle = SmallCircle(pos=(x, y), size=size)
+            small_circles.append(small_circle)
+        return small_circles
+
+    def check_collisions(self):
+        for small_circle in self.small_circles:
+            distance = math.dist((self.player.pos[0] + self.player.size // 2, self.player.pos[1] + self.player.size // 2),
+                                (small_circle.pos[0] + small_circle.size // 2, small_circle.pos[1] + small_circle.size // 2))
+            if distance < (self.player.size + small_circle.size) // 2:
+                self.points += 1
+                self.small_circles.remove(small_circle)
+                self.small_circles.append(SmallCircle(pos=(random.randrange(self.resolution[0]), random.randrange(self.resolution[1])),
+                                                      size=random.randint(10, 30)))
+                if self.points == 10:
+                    self.win = True
+                    self.win_timer = time.time()
+
+    def draw_points(self):
+        font = pygame.font.Font(None, 36)
+        text = font.render(f"Points: {self.points}", True, pygame.Color(255, 255, 255))
+        self.screen.blit(text, (self.resolution[0] - 150, 20))
+
+    def draw_win_message(self):
+        current_time = time.time()
+        if current_time - self.last_flash_time > self.flash_duration:
+            self.last_flash_time = current_time
+            return True
+
+        font = pygame.font.Font(None, 144)
+        rainbow_colors = [pygame.Color(255, 0, 0), pygame.Color(255, 165, 0), pygame.Color(255, 255, 0),
+                          pygame.Color(0, 128, 0), pygame.Color(0, 0, 255), pygame.Color(75, 0, 130),
+                          pygame.Color(238, 130, 238)]
+        text = font.render("You win!", True, random.choice(rainbow_colors))
+        text_rect = text.get_rect(center=(self.resolution[0] // 2, self.resolution[1] // 2))
+        self.screen.blit(text, text_rect)
+
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        running = False
+
+            if not self.win:
+                keys = pygame.key.get_pressed()
+                self.player.move(keys)
+
+                self.check_collisions()
+
+                black = pygame.Color(0, 0, 0)
+                self.screen.fill(black)
+
+                for small_circle in self.small_circles:
+                    small_circle.draw(self.screen)
+
+                self.player.draw(self.screen)
+
+                self.draw_points()
+            else:
+                if self.draw_win_message() and time.time() - self.win_timer > self.win_display_time:
+                    self.win = False
+                    self.points = 0
+                    self.small_circles = self.generate_small_circles(10)
+
+            pygame.display.flip()
+            self.dt = self.clock.tick(60)
+
+        pygame.quit()
+
 def main():
     pygame.init()
-    pygame.display.set_caption("Larger Circular Pixel Move")
-    clock = pygame.time.Clock()
-    dt = 0
-    resolution = (800, 600)
-    screen = pygame.display.set_mode(resolution)
-    player = Player(pos=(resolution[0] // 2, resolution[1] // 2), size=60)  # Adjust the size here
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        keys = pygame.key.get_pressed()
-        player.move(keys)
-
-        black = pygame.Color(0, 0, 0)
-        screen.fill(black)
-        player.draw(screen)
-        pygame.display.flip()
-        dt = clock.tick(60)
-
-    pygame.quit()
+    game = Game((1920, 1080))
+    game.run()
 
 if __name__ == "__main__":
     main()
